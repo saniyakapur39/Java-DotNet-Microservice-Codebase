2a35ba76211088c4b5ac2d72f9450a97d23959b5
saniyakapur39 <56382122+saniyakapur39@users.noreply.github.com>
2026-07-08 10:02:12 -0400

    cobol to java dotnet micro-service

---
+++ common-util/src/main/java/com/hcas/commonutil/util/ErrorCodes.java
@@ -0,0 +1,8 @@
+package com.hcas.commonutil.util;
+
+public class ErrorCodes {
+    public static final String FILE_NOT_FOUND = "ERR-1001";
+    public static final String FILE_OPEN_ERROR = "ERR-1002";
+    public static final String FORMAT_ERROR = "ERR-1003";
+    public static final String PARSE_ERROR = "ERR-1004";
+}
