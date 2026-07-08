793167405b51a1f4ade820f97e0dde52e3b2969f
saniyakapur39 <56382122+saniyakapur39@users.noreply.github.com>
2026-07-08 10:01:54 -0400

    cobol to java dotnet micro-service

---
+++ enrollment-report-service/src/main/java/com/hcas/enrollmentreport/util/ReturnCodeManager.java
@@ -0,0 +1,8 @@
+package com.hcas.enrollmentreport.util;
+
+public class ReturnCodeManager {
+    public static final int SUCCESS = 0;
+    public static final int FILE_OPEN_ERROR = 16;
+    public static final int FORMAT_ERROR = 8;
+    // Add more as needed
+}
