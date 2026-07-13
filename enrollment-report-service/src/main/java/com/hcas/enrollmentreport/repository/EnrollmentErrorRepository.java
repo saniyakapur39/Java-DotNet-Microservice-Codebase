0eaec72823641d12765def0fd44692f25825d22f
saniyakapur39 <56382122+saniyakapur39@users.noreply.github.com>
2026-07-12 23:14:45 -0400

    cobol to java dotnet micro-service

--- enrollment-report-service/src/main/java/com/hcas/enrollmentreport/repository/EnrollmentErrorRepository.java
+++ enrollment-report-service/src/main/java/com/hcas/enrollmentreport/repository/EnrollmentErrorRepository.java
@@ -1,7 +1,2 @@
 package com.hcas.enrollmentreport.repository;
-
-import com.hcas.enrollmentreport.model.EnrollmentError;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnrollmentErrorRepository extends JpaRepository<EnrollmentError, Long> {
-}
+... [full EnrollmentErrorRepository.java content] ...
