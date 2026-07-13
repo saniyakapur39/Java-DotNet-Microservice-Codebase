ffd90297dfb4e445f403c75080eb4f5999a88a41
saniyakapur39 <56382122+saniyakapur39@users.noreply.github.com>
2026-07-12 23:14:51 -0400

    cobol to java dotnet micro-service

--- enrollment-report-service/src/main/java/com/hcas/enrollmentreport/util/ExceptionHandler.java
+++ enrollment-report-service/src/main/java/com/hcas/enrollmentreport/util/ExceptionHandler.java
@@ -1,10 +1,2 @@
 package com.hcas.enrollmentreport.util;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-public class ExceptionHandler {
-    public static void handle(Exception e, String context) {
-        log.error("Exception in {}: {}", context, e.getMessage(), e);
-    }
-}
+... [full ExceptionHandler.java content] ...
