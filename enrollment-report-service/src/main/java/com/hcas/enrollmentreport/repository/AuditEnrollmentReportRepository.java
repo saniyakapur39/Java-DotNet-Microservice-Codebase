2ed89f8023172fbeaf489cec7c2e0f4bf99fba0e
saniyakapur39 <56382122+saniyakapur39@users.noreply.github.com>
2026-07-12 23:14:46 -0400

    cobol to java dotnet micro-service

--- enrollment-report-service/src/main/java/com/hcas/enrollmentreport/repository/AuditEnrollmentReportRepository.java
+++ enrollment-report-service/src/main/java/com/hcas/enrollmentreport/repository/AuditEnrollmentReportRepository.java
@@ -1,10 +1,2 @@
 package com.hcas.enrollmentreport.repository;
-
-import com.hcas.enrollmentreport.model.AuditEnrollmentReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AuditEnrollmentReportRepository extends JpaRepository<AuditEnrollmentReport, Long> {
-    AuditEnrollmentReport findByJobId(UUID jobId);
-}
+... [full AuditEnrollmentReportRepository.java content] ...
