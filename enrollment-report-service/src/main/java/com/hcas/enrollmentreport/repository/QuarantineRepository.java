5b44eb9b0455492ff186a88c967328e925d64850
saniyakapur39 <56382122+saniyakapur39@users.noreply.github.com>
2026-07-12 23:14:47 -0400

    cobol to java dotnet micro-service

--- enrollment-report-service/src/main/java/com/hcas/enrollmentreport/repository/QuarantineRepository.java
+++ enrollment-report-service/src/main/java/com/hcas/enrollmentreport/repository/QuarantineRepository.java
@@ -1,7 +1,2 @@
 package com.hcas.enrollmentreport.repository;
-
-import com.hcas.enrollmentreport.model.QuarantineRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuarantineRepository extends JpaRepository<QuarantineRecord, Long> {
-}
+... [full QuarantineRepository.java content] ...
