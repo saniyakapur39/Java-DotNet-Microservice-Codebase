17504f1c8314f313339c69858576aa67b0f0c933
saniyakapur39 <56382122+saniyakapur39@users.noreply.github.com>
2026-07-12 23:14:59 -0400

    cobol to java dotnet micro-service

--- enrollment-report-service/src/main/java/com/hcas/enrollmentreport/dto/ReportRunDto.java
+++ enrollment-report-service/src/main/java/com/hcas/enrollmentreport/dto/ReportRunDto.java
@@ -1,10 +1,2 @@
 package com.hcas.enrollmentreport.dto;
-
-import lombok.*;
-
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
-public class ReportRunDto {
-    private String jobId;
-    private String status;
-    private String reportPath;
-}
+... [full ReportRunDto.java content] ...
