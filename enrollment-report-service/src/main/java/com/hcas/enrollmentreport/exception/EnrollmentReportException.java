da9f64b29701c890cfd350290c26d453e28c1e55
saniyakapur39 <56382122+saniyakapur39@users.noreply.github.com>
2026-07-12 23:14:52 -0400

    cobol to java dotnet micro-service

--- enrollment-report-service/src/main/java/com/hcas/enrollmentreport/exception/EnrollmentReportException.java
+++ enrollment-report-service/src/main/java/com/hcas/enrollmentreport/exception/EnrollmentReportException.java
@@ -1,7 +1,2 @@
 package com.hcas.enrollmentreport.exception;
-
-public class EnrollmentReportException extends RuntimeException {
-    public EnrollmentReportException(String message) {
-        super(message);
-    }
-}
+... [full EnrollmentReportException.java content] ...
