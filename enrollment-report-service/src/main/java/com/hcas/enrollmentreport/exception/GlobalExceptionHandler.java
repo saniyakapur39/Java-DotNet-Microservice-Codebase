6e0695704ebe32ab3bc100f46cc5a10ea57c05e0
saniyakapur39 <56382122+saniyakapur39@users.noreply.github.com>
2026-07-12 23:14:53 -0400

    cobol to java dotnet micro-service

--- enrollment-report-service/src/main/java/com/hcas/enrollmentreport/exception/GlobalExceptionHandler.java
+++ enrollment-report-service/src/main/java/com/hcas/enrollmentreport/exception/GlobalExceptionHandler.java
@@ -1,13 +1,2 @@
 package com.hcas.enrollmentreport.exception;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.http.ResponseEntity;
-
-@ControllerAdvice
-public class GlobalExceptionHandler {
-    @ExceptionHandler(EnrollmentReportException.class)
-    public ResponseEntity<String> handleEnrollmentReportException(EnrollmentReportException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
-    }
-}
+... [full GlobalExceptionHandler.java content] ...
