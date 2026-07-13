a0220e2f6d5359c1b02fc45415b8264ed75a3922
saniyakapur39 <56382122+saniyakapur39@users.noreply.github.com>
2026-07-12 23:15:35 -0400

    cobol to java dotnet micro-service

--- common-util/src/main/java/com/hcas/commonutil/util/ExceptionMapper.java
+++ common-util/src/main/java/com/hcas/commonutil/util/ExceptionMapper.java
@@ -1,13 +1,2 @@
 package com.hcas.commonutil.util;
-
-public class ExceptionMapper {
-    public static String map(Exception e) {
-        if (e instanceof java.io.FileNotFoundException) {
-            return ErrorCodes.FILE_NOT_FOUND;
-        } else if (e instanceof java.io.IOException) {
-            return ErrorCodes.FILE_OPEN_ERROR;
-        } else {
-            return ErrorCodes.FORMAT_ERROR;
-        }
-    }
-}
+... [full ExceptionMapper.java content] ...
